4a9e525ec52706c608c0668befe4ac56c995b89e
almeidaGi <gialmeida899@gmail.com>
2022-02-17 22:43:42 -0300

    Criado category Model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,9 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.module";
 
 export class InMemoryDatabase implements InMemoryDbService{
     createDb(){
-        const categories = 
+        const categories: Category []= 
         [
             {id:1, name:'Saia', description: 'Saia Midi'},            
             {id:2, name:'Calças', description: 'Calças Jeans'},            
