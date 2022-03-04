c07beb28ba3deba015ac80c2e168dcf8f355e325
almeidaGi <gialmeida899@gmail.com>
2022-03-04 15:19:53 -0300

    Adicionando metodos base para CategoryFomrs

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -6,10 +6,11 @@ export class InMemoryDatabase implements InMemoryDbService{
         const categories: Category [] = 
         [
             {id:1, name:'Saia', description: 'Saia Midi'},            
-            {id:2, name:'Calças', description: 'Calças Jeans'},            
+            {id:2, name:'Calça', description: 'Calças Jeans'},            
             {id:3, name:'Top', description: 'Top Viscose'},
-            {id:4, name:'shorts', description: 'Shorts feminino Jeans'} ,              
-            {id:5, name:'Calças', description: 'Viscose'}
+            {id:4, name:'Shorts', description: 'Shorts feminino Jeans'} ,              
+            {id:5, name:'Calça', description: 'Viscose'},
+            {id:6, name:'Calça', description: 'Moletom'}
         ]
         return {categories}
     }
