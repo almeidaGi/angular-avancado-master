633c64760985eb017726ab8b68774744ba67f9c7
almeidaGi <gialmeida899@gmail.com>
2022-04-09 16:51:45 -0300

    Implementada baseResouceService e refatorada categoryServie

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -1,64 +1,12 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { catchError, map, Observable, throwError } from 'rxjs';
+import { Injectable, Injector } from '@angular/core';
+import { baseResouceService } from 'src/app/shared/services/base-resource.service';
 import { Category } from './category.model';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
-  private apiPath: string = "api/categories"
-
-  constructor(private http: HttpClient) { }
-
-  getAll(): Observable<Category[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategories)
-    )
-  }
-  getById(id: number): Observable<Category> {
-  
-    const url = `${this.apiPath}/${id}`;
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToCategory)
-    )
-  }
-  create(category: Category): Observable<Category> {
-    return this.http.post(this.apiPath, category).pipe(      
-      map(this.jsonDataToCategory),
-      catchError(this.handleError)
-    )
-  }
-  update(category: Category): Observable<Category>{
-
-    const url = `${this.apiPath}/${category.id}`;
-    return  this.http.put( url, category).pipe(
-      catchError(this.handleError),
-      map(()=> category)
-    )
-  }
-  delete(id: number): Observable<any>{    
-    const url = `${this.apiPath}/${id}`;
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(() => null)
-    )
-  }
-
-  private jsonDataToCategories(jsonData: any[]): Category[] {  
-  
-    const cetegories: Category[] = [];
-    jsonData.forEach(element => cetegories.push(element as Category));
-    return cetegories;
-  }
-  private handleError(error: any): Observable<any> {   
-    console.log("ERRO NA REQUISIÇÃO =>", error);
-    return throwError(error);
-  }
-  private jsonDataToCategory(jsonData: any): Category {
-    return jsonData as Category;
-  }
+export class CategoryService extends baseResouceService<Category>{
+  constructor(injector: Injector) 
+  {super('api/categories', injector) }
 }
