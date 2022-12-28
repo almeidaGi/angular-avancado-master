46b6201e1f3ecea1556d05d8709fbf5f24074846
Gii <gialmeida899@gmail.com>
2022-12-28 18:06:32 -0300

    adding  report templat

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -6,5 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent {
-
+  pageTitle = "Relatórios de despesas"
+  public date = new Date();
+  public year = +this.date.getUTCFullYear();  
+  public pastYears = +this.year - 5;  
+  public yearList: Array<any> = [];
+  constructor()
+  {  
+   for (let index = this.pastYears; index <= this.year; index++) {   
+    this.yearList.push({
+      value: index ,
+      description: index});
+   }
+   this.yearList.reverse() 
+  }
+ public meses = [
+    {
+      value: '',
+      description: 'Selecione um mês'
+    },
+    {
+      value: 1,
+      description: 'Janeiro'
+    },
+    {
+      value: 2,
+      description: 'Fevereiro'
+    },
+    {
+      value: 3,
+      description: 'Fevereiro'
+    },
+    {
+      value: 4,
+      description:  'Abril'
+    },
+    {
+      value: 5,
+      description:   'Maio'
+    },
+    {
+      value: 6,
+      description:   'Junho'
+    },
+    {
+      value: 7,
+      description:   'Julho'
+    },
+    {
+      value: 8,
+      description: 'Agosto'
+    },
+    {
+      value: 9,
+      description: 'Setembro'
+    },
+    {
+      value: 10,
+      description: 'Outubro'
+    },
+    {
+      value: 11,
+      description: 'Novembro'
+    },
+    {
+      value: 12,
+      description: 'Dezembro'
+    },
+  ]
+public generateReports(){}
 }
